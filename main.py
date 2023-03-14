592e628f309a87dd5355a975fc3b7b7521f6977c
Szymon Mikitiuk <szmiki@softserveinc.com>
2023-03-14 21:40:47 +0100

    Implement concurrent database ping

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import psycopg2
+
 from config import config
 
 
-def main():
+def fetch_data() -> None:
     with psycopg2.connect(**config.dict()) as connection:
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM users;")
@@ -10,9 +13,31 @@ def main():
             print(omg)
 
 
+def postgres_ping(connection_index: int) -> None:
+    wrong_connection = {
+        **config.dict(),
+        'password': 'omg'
+    }
+
+    try:
+        print(f'Connection attempt #{connection_index + 1}')
+        psycopg2.connect(**wrong_connection)
+    except psycopg2.OperationalError as e:
+        print(e)
+
+
+def main() -> None:
+    with ThreadPoolExecutor(max_workers=20) as executor:
+
+        futures = [executor.submit(postgres_ping, index) for index in range(2100)]
+        for future in as_completed(futures):
+            future.result()
+
+
 if __name__ == '__main__':
     main()
-
+    # fetch_data()
 
 
 # TODO create pre-commit config yaml file
+# TODO add requirements.txt as well to minimize dependency footprint
