098fcab28c5887f88bb43df442ce30e91a98aa8c
Szymon Mikitiuk <szmiki@softserveinc.com>
2023-03-16 00:47:04 +0100

    Main module code cleanup

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
+import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from dataclasses import asdict, dataclass
+from typing import Dict
 
 import psycopg2
 
-from config import config
-
 LONG_PASSWORD = 'gibberish' * 100
 PG_OVERLOAD_MSG = 'sorry, too many clients already'
+THREAD_COUNT = 20
 
 
-def fetch_data() -> None:
-    with psycopg2.connect(**config.dict()) as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM users;")
-        for omg in cursor:
-            print(omg)
+@dataclass(frozen=True)
+class Config:
+    database: str
+    user: str
+    password: str
+    host: str
+    port: str
+
+    @classmethod
+    def load_values(cls):
+        return cls(
+            database=os.environ['PGDATABASE'],
+            user=os.environ['PGUSER'],
+            password=os.environ['PGPASSWORD'],
+            host=os.environ['PGHOST'],
+            port=os.environ['PGPORT'],
+        )
+
+    def dict(self) -> Dict:
+        return asdict(self)
 
 
-def postgres_ping(connection_index: int) -> None:
-    wrong_connection = {
-        **config.dict(),
-        'password': LONG_PASSWORD,
-    }
+config = Config.load_values()
+
+
+def postgres_ping() -> None:
+    wrong_connection = {**config.dict(), 'password': LONG_PASSWORD}
 
     try:
-        # print(f'Connection attempt #{connection_index + 1}')
         psycopg2.connect(**wrong_connection)
     except psycopg2.OperationalError as e:
         if PG_OVERLOAD_MSG in str(e):
@@ -32,12 +47,14 @@ def postgres_ping(connection_index: int) -> None:
 
 def main() -> None:
     while True:
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            futures = [executor.submit(postgres_ping, index) for index in range(20)]
+        with ThreadPoolExecutor(max_workers=THREAD_COUNT) as executor:
+            futures = [executor.submit(postgres_ping) for _ in range(THREAD_COUNT)]
             for future in as_completed(futures):
                 future.result()
 
 
 if __name__ == '__main__':
-    main()
-    # fetch_data()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Exiting at your request')
