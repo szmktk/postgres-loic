633363b925f8a1265a9eba2ccbf718d6a2a1c503
Szymon Mikitiuk <szmiki@softserveinc.com>
2023-03-15 14:15:03 +0100

    Add top-level while loop

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import psycopg2
 
 from config import config
 
+LONG_PASSWORD = 'gibberish' * 100
+PG_OVERLOAD_MSG = 'sorry, too many clients already'
+
 
 def fetch_data() -> None:
     with psycopg2.connect(**config.dict()) as connection:
@@ -16,28 +19,25 @@ def fetch_data() -> None:
 def postgres_ping(connection_index: int) -> None:
     wrong_connection = {
         **config.dict(),
-        'password': 'omg'
+        'password': LONG_PASSWORD,
     }
 
     try:
-        print(f'Connection attempt #{connection_index + 1}')
+        # print(f'Connection attempt #{connection_index + 1}')
         psycopg2.connect(**wrong_connection)
     except psycopg2.OperationalError as e:
-        print(e)
+        if PG_OVERLOAD_MSG in str(e):
+            print(e)
 
 
 def main() -> None:
-    with ThreadPoolExecutor(max_workers=20) as executor:
-
-        futures = [executor.submit(postgres_ping, index) for index in range(2100)]
-        for future in as_completed(futures):
-            future.result()
+    while True:
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            futures = [executor.submit(postgres_ping, index) for index in range(20)]
+            for future in as_completed(futures):
+                future.result()
 
 
 if __name__ == '__main__':
     main()
     # fetch_data()
-
-
-# TODO create pre-commit config yaml file
-# TODO add requirements.txt as well to minimize dependency footprint
